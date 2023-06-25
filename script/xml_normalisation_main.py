999217ebb4de1f82bb5f5342907e1f20317fdd35
Ereshkigall <Ishi.thelema@gmail.com>
2023-06-25 18:54:29 +0200

    added TODO folder with xml

--- script/xml_normalisation_main.py
+++ script/xml_normalisation_main.py
@@ -8,10 +8,11 @@ test_BM_file:str = "../tests/Mazarinades_tests/Bibliotheque_Mazarine/BM48896_MAZ
 #normalise_xml(input_filepath=test_file6, output_filepath="temp_xml/temp.xml")
 #normalise_xml(input_filepath=test_file7, output_filepath="temp_xml/temp.xml")
 #tei_to_json_file(filepath="temp_xml/temp.xml", main_output_dir="./temp")
-#normalise_xml_dir(dir_path=test_dir, value_to_change="pièce de théâtre", replacement="texte de forme théâtrale", change_name=True)
 #normalise_names(input_filepath=test_file8, output_filepath="temp_xml/temp.xml", value_to_change="pièce de théâtre", replacement="texte de forme théâtrale")
 #tei_to_json_file(filepath="temp_xml/temp.xml", main_output_dir="./temp")
 #test_stats()
 
 #change_attribute_name(input_filepath=test_BM_file, output_filepath="temp_xml/BM.xml", tag="pb", old_attribute="n", new_attribute="vue", namespace=namespace)
-change_attribute_name_dir(dir_path=maz_dir, tag="pb", old_attribute="n", new_attribute="vue", namespace=namespace)
+#change_attribute_name_dir(dir_path=maz_dir, tag="pb", old_attribute="n", new_attribute="vue", namespace=namespace)
+
+normalise_xml_dir(dir_path=test_dir, value_to_change="pièce de théâtre", replacement="texte de forme théâtrale", change_name=True)
