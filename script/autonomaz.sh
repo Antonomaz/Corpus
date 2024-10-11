1952b8c5e52ef540292d7ec48dad370e71ee4b57
Marceau <git@marceau-h.fr>
2024-10-11 15:03:44 +0200

    Create autonomaz.sh

---
+++ script/autonomaz.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+cd ..
+
+source .env
+
+old_head=$(git rev-parse HEAD)
+
+git pull
+
+IFS=$'\0'
+
+changed_files=()
+while IFS= read -r -d $'\0'; do
+    changed_files+=("$REPLY")
+done < <(git diff --find-renames --name-only -z $old_head HEAD)
+
+unset IFS
+
+for file in "${changed_files[@]}"; do
+    if [[ "$file" == *.xml && "$file" == Mazarinades* ]]; then
+        echo "Fichier XML Mazarinades modifié: $file"
+        echo "Lancement du curl magique"
+        file_name=$(basename $file)
+        curl -vX PUT -H "Content-Type: application/xml" --data "@$file" $url_anto/$file_name -u $mdp_anto
+    fi
+done
